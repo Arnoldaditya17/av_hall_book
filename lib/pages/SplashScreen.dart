a7b72b5d7d6c70baac17ec23d3b7fa1b30dac9bc
Arnoldaditya17 <adityasinghrajput02@gmail.com>
2023-11-07 15:50:35 +0530

    initial commit

---
+++ lib/pages/SplashScreen.dart
@@ -0,0 +1,52 @@
+
+import 'package:av_hall_book/pages/LoginPage.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetologinscreen();
+  }
+
+  _navigatetologinscreen() async {
+    await Future.delayed(const Duration(milliseconds: 1500), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const LoginPage(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/image4.jpg',
+              width: 200,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const  Text('Welcome To Av Hall Booking ',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))
+          ],
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
